e6ed569254ca5aed279dddc05c840f3cbac72ab4
Collins Sirwani <sircollo71@gmail.com>
2022-04-11 17:34:14 +0300

    Generate components

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MealComponent } from './meal/meal.component';
+import { MealDetailsComponent } from './meal-details/meal-details.component';
+import { MealFormComponent } from './meal-form/meal-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MealComponent,
+    MealDetailsComponent,
+    MealFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
